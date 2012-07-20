acca974676bcad6b3fb4a3af6af21a8e12fc9d8b
Jabara <jabaraster@gmail.com>
2012-07-20 12:54:42 +0900

    mainte.

--- src/main/java/jabara/web/rest/BlackoutScheduleResource.java
+++ src/main/java/jabara/web/rest/BlackoutScheduleResource.java
@@ -3,16 +3,15 @@
  */
 package jabara.web.rest;
 
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.io.IOUtils;
 
@@ -22,18 +21,33 @@ import org.apache.commons.io.IOUtils;
 @Path("/blackout/schedule")
 public class BlackoutScheduleResource {
 
+    private static final String ENCODING      = "UTF-8";    //$NON-NLS-1$
+    private static final String QDEN_ENCODING = "Shift_JIS"; //$NON-NLS-1$
+
     /**
      * @return 計画停電スケジュール.
-     * @throws MalformedURLException
      * @throws IOException
      */
     @SuppressWarnings("static-method")
-    @Produces({ MediaType.TEXT_PLAIN })
+    @Produces({ "text/plain; charset=" + ENCODING })
     @GET
-    public String getExpandedSchedule() throws MalformedURLException, IOException {
-        final InputStream in = new URL("http://www2.kyuden.co.jp/kt_search/csv/201207.csv").openStream(); //$NON-NLS-1$
-        final ByteArrayOutputStream mem = new ByteArrayOutputStream();
-        IOUtils.copy(in, mem);
-        return new String(mem.toByteArray(), "Shift_JIS"); //$NON-NLS-1$
+    public byte[] getExpandedSchedule() throws IOException {
+        final StringBuilder ret = new StringBuilder();
+        append(ret, "201207.csv"); //$NON-NLS-1$
+        append(ret, "201208.csv"); //$NON-NLS-1$
+        append(ret, "201209.csv"); //$NON-NLS-1$
+        return new String(ret).getBytes(ENCODING);
+    }
+
+    private static void append(final StringBuilder pRet, final String pCsvFileName) throws IOException {
+        final InputStream in = new URL("http://www2.kyuden.co.jp/kt_search/csv/" + pCsvFileName).openStream(); //$NON-NLS-1$
+        try {
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(in, QDEN_ENCODING));
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+
+            }
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
     }
 }
